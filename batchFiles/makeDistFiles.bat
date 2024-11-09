8be8a82428ecc2035d8a0179f9355fd3b65ba713
siejwkaodj <june123000@naver.com>
2024-11-10 01:15:43 +0900

    update/dist: SEO관련 항목 설정 - sitemap, favicon, og-image / makedistfile batch file 수정

--- batchFiles/makeDistFiles.bat
+++ batchFiles/makeDistFiles.bat
@@ -1,6 +1,9 @@
 @echo off
 echo ========== 2. Copying files to dist/ ==========
 xcopy index.html dist\ /Y
+xcopy favicon.ico dist\ /Y
+xcopy og-image.png dist\ /Y
+xcopy sitemap.xml dist\ /Y
 xcopy src\main.js dist\src\ /Y
 xcopy src\output.css dist\src\ /Y
 echo ========== Files copied successfully ==========
