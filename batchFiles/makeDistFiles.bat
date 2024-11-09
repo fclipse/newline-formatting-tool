37687998619b72a1686c1d7abb7d44df6a62e17f
siejwkaodj <june123000@naver.com>
2024-11-10 01:23:10 +0900

    update/dist:robots.txt 빠진거 추가 + bat파일 수정

--- batchFiles/makeDistFiles.bat
+++ batchFiles/makeDistFiles.bat
@@ -4,6 +4,7 @@ xcopy index.html dist\ /Y
 xcopy favicon.ico dist\ /Y
 xcopy og-image.png dist\ /Y
 xcopy sitemap.xml dist\ /Y
+xcopy robots.txt dist\ /Y
 xcopy src\main.js dist\src\ /Y
 xcopy src\output.css dist\src\ /Y
 echo ========== Files copied successfully ==========
